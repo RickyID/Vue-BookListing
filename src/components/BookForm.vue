84fea19264950e3035e654b9a88a648eea02ca24
Ricky Garcia <ricky.g@purple.com>
2021-07-07 16:41:19 -0600

    Set's up component communication

---
+++ src/components/BookForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="" action="#" method="post"  @submit.prevent="onSubmit(bookTitle,bookAuthor)">
+      <input type="text" v-model="bookTitle" name="title" value="" placeholder="Book Title">
+      <input type="text" v-model="bookAuthor" name="author" value="" placeholder="Book Author">
+      <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "BookForm",
+  props: ['books'],
+  data() {
+    return {
+      bookTitle: '',
+      bookAuthor: '',
+    }
+  },
+  methods: {
+    bookSubmit: function (bookTitle, bookAuthor) {
+      this.$emit('addBook', bookTitle, bookAuthor);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
